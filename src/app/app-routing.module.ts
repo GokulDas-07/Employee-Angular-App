2208ef64eb8ef04990b09179a9d3fa7877ab66a7
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-24 16:16:29 +0530

    addemployee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
 
 const routes: Routes = [
-  {path:"",component:AdminLoginComponent}
+  {path:"",component:AdminLoginComponent},
+  {path:"add",component:EmployeeAddComponent}
 ];
 
 @NgModule({
