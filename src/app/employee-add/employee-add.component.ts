51a3846928e1f27ef778b4ef7ef264455e309921
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-28 12:44:05 +0530

    values added

--- src/app/employee-add/employee-add.component.ts
+++ src/app/employee-add/employee-add.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-add.component.css']
 })
 export class EmployeeAddComponent {
+  name=""
+  designation=""
+  Salary=""
+  company=""
 
+  readValues=()=>
+  {
+
+  let data:any={
+    "name":this.name,
+    "designation":this.designation,
+    "Salary":this.Salary,
+    "company":this.company
+  }
+  console.log(data)
+
+
+}
 }
